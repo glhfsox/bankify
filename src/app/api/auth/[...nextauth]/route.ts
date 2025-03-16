887ee8e540774636260acde916b3eb478226a6b3
Artem Pashchenko <artemmorzh@gmail.com>
2025-03-16 21:07:31 +0100

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
+const handler = NextAuth(authConfig);
+export {handler as GET , handler as POST };
+
 
 export const authOptions = {
   providers: [
